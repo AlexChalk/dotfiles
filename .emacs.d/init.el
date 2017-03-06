2b13c7190afe34c6cb4fc7e1517fa8afc59eee2b
Alex Chalk <alexchalk17@gmail.com>
2017-03-06 07:52:43 -0500

    Add .gitignore to repo.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -227,4 +227,22 @@
 
 ;;;Emacs custom-set-* settings (Emacs advises that you leave these alone!)
 (custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(powerline-active1 ((t (:background "#073642" :foreground "#93a1a1")))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(display-buffer-alist
+   (quote
+    (("\\*magit: .*" display-buffer-same-window)
+     ("\\*ruby\\*.*" display-buffer-same-window)
+     ("\\*ansi-term\\*" display-buffer-same-window)
+     ("\\*shell\\*" display-buffer-same-window))))
+ '(package-selected-packages
+   (quote
+    (use-package smartparens rvm rainbow-delimiters powerline magit inf-ruby helm-projectile flycheck evil-surround evil-leader dumb-jump color-theme-sanityinc-solarized auto-complete))))
