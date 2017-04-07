7c128d8f9d183243777f2b42272745ddcd7c454d
Alex Chalk <alexchalk17@gmail.com>
2017-04-06 21:50:50 -0400

    Add <> pair.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -148,6 +148,7 @@
   :config
   (sp-pair "'" nil :actions :rem)
   (sp-pair "\"" nil :actions :rem)
+  (sp-pair "<" ">")
   (sp-local-pair 'web-mode "%" "%" :wrap "M-%")
   (sp-local-pair 'emacs-lisp-mode "'" nil :actions nil)
   (sp-local-pair 'markdown-mode "`" nil :actions '(insert))  
