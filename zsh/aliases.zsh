e1313e9ab686b4e2981eaa84d56edb364594b6ec
Alex Chalk <alexchalk17@gmail.com>
2017-07-21 21:31:43 -0400

    Add aliases
    -Gitconfig: add 'diff' aliases.
    -Zsh: add 'clear' alias.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,3 +5,4 @@ alias magit="emacs -q -l ~/dotfiles/emacs.d/magit-init.el"
 alias tree='tree -C -I "*~|*#|node_modules|sprockets"'
 alias chrome='open -a "Google Chrome"'
 alias wdio='./node_modules/.bin/wdio wdio.conf.js'
+alias c='clear'
