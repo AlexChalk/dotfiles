ee9b71a271d658db294e2868be272c60734077b7
Alexander Chalk <alexchalk17@gmail.com>
2018-08-04 19:55:57 -0400

    Add automated script for building symlinks on new machine

---
+++ build-symlinks.sh
@@ -0,0 +1,16 @@
+cd $HOME
+ln -s dotfiles/zshrc .zshrc
+ln -s dotfiles/tmux.conf .tmux.conf
+ln -s dotfiles/ignore .ignore
+ln -s dotfiles/gitconfig .gitconfig
+ln -s dotfiles/gitignore_global .gitignore_global
+ln -s dotfiles/rubocop.yml .rubocop.yml
+mkdir .lein
+ln -s dotfiles/lein/profiles.clj .lein/profiles.clj
+mkdir -p .config/nvim
+ln -s dotfiles/nvim/init.vim .config/nvim/init.vim
+mkdir .config/nvim/Ultisnips
+mkdir .config/nvim/ftdetect
+ln -s dotfiles/nvim/ftdetect/* .config/nvim/ftdetect
+ln -s dotfiles/nvim/Ultisnips/javascript.snippets .config/nvim/Ultisnips
+ln -s dotfiles/nvim/Ultisnips/javascript.snippets .config/nvim/Ultisnips/typescript.snippets
