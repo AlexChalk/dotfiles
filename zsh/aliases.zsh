bde2e720b6d50f3ba63b0a4e04aaaa8460a263e2
Alexander Chalk <alexchalk17@gmail.com>
2018-08-28 16:18:09 -0400

    Add quick way to copy a busbud app name to clipboard

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -13,4 +13,5 @@ alias ag='ag --path-to-ignore $HOME/.ignore'
 alias unix-linebreaks="vim -u NONE -c 'e ++ff=dos' -c 'w ++ff=unix' -c q"
 alias hjoin="heroku apps -t busbud | tail -n+2 | grep . | sed 's/ .*$//' | fzf --header='Select the app you want to join' | xargs heroku join -a"
 alias hlogs="heroku apps --all | tail -n+2 | grep . | sed 's/ .*$//' | fzf --header='Select the app whose logs you want to tail' | xargs heroku logs -t -a"
+alias hname="heroku apps --all | tail -n+2 | grep . | sed 's/ .*$//' | fzf --header='Select the app whose name you want to copy' | tr -d '\n' | pbcopy"
 alias nuse="n \$(cat .nvmrc)"
