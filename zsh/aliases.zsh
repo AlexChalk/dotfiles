46ec2027f5b541e151a92bdc0f8ee5585ab34610
Alexander Chalk <alexchalk17@gmail.com>
2018-10-30 21:55:07 -0400

    Modify haskell config

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -15,3 +15,6 @@ alias hlogs="heroku apps --all | tail -n+2 | grep . | sed 's/ .*$//' | fzf --hea
 alias hname="heroku apps --all | tail -n+2 | grep . | sed 's/ .*$//' | fzf --header='Select the app whose name you want to copy' | tr -d '\n' | pbcopy"
 alias nuse="n \$(cat .nvmrc)"
 alias antibodyupdate='antibody bundle < $HOME/dotfiles/zsh_plugins.txt | sed -e "s:$HOME:\$HOME:g" > $HOME/.zsh_plugins.sh && antibody update'
+# alias ghc="stack ghc"
+# alias ghci="stack ghci"
+# alias runghc="stack runghc"
