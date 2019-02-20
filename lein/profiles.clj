2e5d9cfc3e2bdd48b8632974b0af8a61660fcbec
Alexander Chalk <contact@alexchalk.net>
2019-02-20 18:40:00 -0500

    Update clojure(script) config

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,4 +1,11 @@
-{:user {:plugins [[venantius/ultra "0.5.3"]
-                  [cider/cider-nrepl "0.18.0"]
-                  [lein-midje "3.2.1"]]
-        :middleware [ultra.plugin/middleware cider-nrepl.plugin/middleware]}}
+{:user {:plugins [[cider/cider-nrepl "0.21.0"]
+                  [venantius/ultra "0.5.4"]
+                  [com.billpiel/sayid "0.0.17"]
+                  [com.jakemccrary/lein-test-refresh "0.23.0"]]
+        :dependencies [[vvvvalvalval/scope-capture "0.3.2"]
+                       [pjstadig/humane-test-output "0.9.0"]]
+        ;; disable when support is released for nrepl
+        :ultra {:repl false}
+        :injections [(require 'sc.api)
+                     (require 'pjstadig.humane-test-output)
+                     (pjstadig.humane-test-output/activate!)]}}
