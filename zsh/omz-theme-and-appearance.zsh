8214b5be1530be2fefd19c8353f3a3c34bf7389a
Alexander Chalk <contact@alexchalk.net>
2019-07-05 21:56:14 -0400

    Support linux in theming

--- zsh/omz-theme-and-appearance.zsh
+++ zsh/omz-theme-and-appearance.zsh
@@ -4,4 +4,17 @@ autoload -U colors && colors
 # Enable ls colors
 export LSCOLORS="Gxfxcxdxbxegedabagacad"
 
-alias ls='ls -G'
+if [[ "$OSTYPE" == darwin* ]]; then
+  # this is a good alias, it works by default just using $LSCOLORS
+  ls -G . &>/dev/null && alias ls='ls -G'
+else
+  # For GNU ls, we use the default ls color theme. They can later be overwritten by themes.
+  if [[ -z "$LS_COLORS" ]]; then
+    (( $+commands[dircolors] )) && eval "$(dircolors -b)"
+  fi
+
+  ls --color -d . &>/dev/null && alias ls='ls --color=tty' || { ls -G . &>/dev/null && alias ls='ls -G' }
+
+  # Take advantage of $LS_COLORS for completion as well.
+  zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
+fi
