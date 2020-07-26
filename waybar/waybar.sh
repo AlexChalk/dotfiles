c6a17921e8fdc091c9bb3ccaa7f8ac79cb455158
Alex Chalk <contact@alexchalk.net>
2020-07-26 13:39:41 -0400

    fix: Fix pulse startup/waybar reload

--- waybar/waybar.sh
+++ waybar/waybar.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Terminate already running bar instances
-killall -q waybar
+killall -q .waybar-wrapped
 
 # Wait until the processes have been shut down
 while pgrep -x waybar >/dev/null; do sleep 1; done
