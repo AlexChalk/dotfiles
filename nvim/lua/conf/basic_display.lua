8b07fabe6bbcaf556869da231b0a8f088e047a65
Alex Chalk <contact@alexchalk.net>
2021-08-02 01:32:22 -0400

    feat: Convert first file

---
+++ nvim/lua/conf/basic_display.lua
@@ -0,0 +1,26 @@
+-----------------------------------------------------
+-- Display
+-----------------------------------------------------
+local opt = vim.opt
+opt.ruler = true            -- show the cursor position all the time
+opt.incsearch = true        -- do incremental searching
+opt.number = true           -- set numbers behaviour
+opt.relativenumber = true   -- set numbers behaviour
+
+opt.inccommand = 'nosplit'  -- show results of command incrementally
+opt.textwidth = 85          -- Make it obvious where 
+opt.colorcolumn = '+1'      -- 85 characters is
+opt.splitbelow = true       -- set default :sp position
+opt.splitright = true       -- set default :vsp position
+opt.cursorline = true
+
+
+function ToggleTextWidth()
+  if vim.bo.textwidth == 0 then
+    opt.textwidth=85
+  else
+    opt.textwidth=0
+  end
+end
+
+vim.cmd("command! ToggleTextWidth lua ToggleTextWidth()")
--- nvim/lua/conf/start.lua
+++
@@ -1,3 +0,0 @@
-vim.opt.relativenumber = false
-vim.opt.syntax = 'off'
-vim.opt.textwidth = 100
