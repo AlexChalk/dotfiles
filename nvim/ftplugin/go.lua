ca14fecc975d77fc90b3f225ab580c58ab7eed0f
Alex Chalk <contact@alexchalk.net>
2021-08-02 19:53:53 -0400

    feat: Delete weird go mapping

--- nvim/ftplugin/go.lua
+++ nvim/ftplugin/go.lua
@@ -2,4 +2,3 @@
 -- Go
 -----------------------------------------------------
 vim.bo.expandtab = false
-vim.api.nvim_buf_set_keymap(0, 'n', '<Leader>sir', '($v%J%ds(0i:<esc>V<leader>sr3u', { noremap = false })
