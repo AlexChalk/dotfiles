90ca25cbe668c899c2dbdd3e62acbd6a7a620948
Alex Chalk <contact@alexchalk.net>
2021-11-19 10:26:11 -0500

    Migrate haskell.vim

---
+++ nvim/lua/conf/haskell.lua
@@ -0,0 +1,17 @@
+-- vim.g.haskell_classic_highlighting = true
+-- vim.g.haskell_indent_if = 3
+-- vim.g.haskell_indent_case = 10
+-- vim.g.haskell_indent_let = 4
+-- vim.g.haskell_indent_where = 6
+-- vim.g.haskell_indent_before_where = 2
+-- vim.g.haskell_indent_after_bare_where = 2
+-- vim.g.haskell_indent_do = 3
+-- vim.g.haskell_indent_in = 1
+-- vim.g.haskell_indent_guard = 2
+-- vim.g.haskell_indent_case_alternative = true
+-- vim.g.cabal_indent_section = 2
+
+-- stack build --copy-compiler-tool hdevtools (plus maybe) hlint hindent hfmt
+-- So you can run stack --stack-yaml stack-<version>.yaml build --copy-compiler-tool in hie dir to install hie for various versions of GHC.
+
+-- https://github.com/haskell/haskell-language-server
