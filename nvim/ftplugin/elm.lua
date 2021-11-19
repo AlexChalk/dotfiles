e5e73a2f871147fb58a1f6ea4082968d970a1112
Alex Chalk <contact@alexchalk.net>
2021-11-19 10:12:13 -0500

    Migrate elm config

---
+++ nvim/ftplugin/elm.lua
@@ -0,0 +1,8 @@
+vim.g.elm_format_autosave = false
+vim.g.elm_setup_keybindings = false
+
+vim.api.nvim_buf_set_keymap(0, "", "K", ":ElmShowDocs<cr>", { noremap = false })
+vim.api.nvim_buf_set_keymap(0, "n", "<leader>sbr", [[vipk<C-v>$A<space>\<esc>vip<leader>sru]], { noremap = false })
+
+vim.api.nvim_set_keymap("n", "p", "p", { noremap = true })
+vim.api.nvim_set_keymap("n", "P", "P", { noremap = true })
