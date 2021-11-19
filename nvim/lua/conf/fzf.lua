ac236bb313870a2d9c499565f0a69fdf0eeea665
Alex Chalk <contact@alexchalk.net>
2021-11-19 14:59:14 -0500

    Migrate fzf config

---
+++ nvim/lua/conf/fzf.lua
@@ -0,0 +1,10 @@
+-----------------------------------------------------
+-- FZF
+-----------------------------------------------------
+-- Respect $HOME/.ignore
+vim.api.nvim_set_keymap("n", "<c-p>", ":Files<CR>", { noremap = true })
+
+-- Search everything
+vim.api.nvim_set_keymap("n", "<leader><c-p>", [[:lua vim.fn['fzf#vim#files']('', {source = 'rg --files --no-ignore --hidden --glob "!.git/*"'})<CR>]], { noremap = true })
+
+vim.env.FZF_DEFAULT_COMMAND = 'rg --files --hidden --follow'
