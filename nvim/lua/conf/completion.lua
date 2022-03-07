1ef6f9626e03380613587ab075f6cfb7ea579599
Alex Chalk <contact@alexchalk.net>
2022-03-07 18:41:18 -0500

    Progress! ~~But command line still doesn't work~~
    Actually it does!

--- nvim/lua/conf/completion.lua
+++ nvim/lua/conf/completion.lua
@@ -15,205 +15,97 @@
 --
 local cmp = require("cmp")
 local luasnip = require("luasnip")
-local rep = require("luasnip.extras").rep
-local s = luasnip.s
-local t = luasnip.t
-local i = luasnip.i
 
 -- Manually trigger completion info
 -- https://github.com/neoclide/coc.nvim/wiki/Using-snippets (old way)
 -- https://github.com/hrsh7th/nvim-cmp/search?q=trigger+completion+is%3Aissue&type=issues
 -- https://github.com/hrsh7th/nvim-cmp/issues/178
 
-local javascript_snippets = {
-  s({ dscr = "{}", trig = "{}" }, { t({ "{", "  " }), i(0), t({ "", "}" }) }),
-  s({ dscr = "();", trig = "();" }, { t({ "(", "  " }), i(0), t({ "", ");" }) }),
-  s({ dscr = "{};", trig = "{};" }, { t({ "{", "  " }), i(0), t({ "", "};" }) }),
-  s({ dscr = "{});", trig = "{});" }, { t({ "{", "  " }), i(0), t({ "", "});" }) }),
-  s(
-    { dscr = "{}));", trig = "{}));" },
-    { t({ "{", "  " }), i(0), t({ "", "}));" }) }
-  ),
-  s({ dscr = "{},", trig = "{}," }, { t({ "{", "  " }), i(0), t({ "", "}," }) }),
-  s({ dscr = "('", trig = "('" }, { t({ "('" }), i(0), t({ "');" }) }),
-  s(
-    { dscr = "req", trig = "req" },
-    { t({ 'require("' }), i(0, "module"), t({ '");' }) }
-  ),
-  s({ dscr = "Describe", trig = "des" }, {
-    t({
-      'describe("',
-    }),
-    i(1, "description"),
-    t({ '", () => {', "  " }),
-    i(0),
-    t({ "", "});" }),
-  }),
-  s({ dscr = "it async", trig = "it" }, {
-    t({
-      'it("',
-    }),
-    i(1, "description"),
-    t({ '", async () => {', "  " }),
-    i(0),
-    t({ "", "});" }),
-  }),
-  s({ dscr = "it", trig = "its" }, {
-    t({
-      'it("',
-    }),
-    i(1, "description"),
-    t({ '", () => {', "  " }),
-    i(0),
-    t({ "", "});" }),
-  }),
-  s({ dscr = "before each", trig = "bef" }, {
-    t({ "beforeEach(() => {", "  " }),
-    i(0),
-    t({ "", "});" }),
-  }),
-  s({ dscr = "after each", trig = "af" }, {
-    t({ "afterEach(() => {", "  " }),
-    i(0),
-    t({ "", "});" }),
-  }),
-  s({ dscr = "before each async", trig = "befa" }, {
-    t({ "beforeEach(async () => {", "  " }),
-    i(0),
-    t({ "", "});" }),
-  }),
-  s({ dscr = "after each async", trig = "afa" }, {
-    t({ "afterEach(async () => {", "  " }),
-    i(0),
-    t({ "", "});" }),
-  }),
-  s({ dscr = "sfc", trig = "sfc" }, {
-    t({
-      "import React from 'react';",
-      "import PropTypes from 'prop-types';",
-      "",
-      "const ",
-    }),
-    i(1, "Component"),
-    t({ " = ({ " }),
-    i(2, "props"),
-    t({ " }) => {", "  " }),
-    i(0),
-    t({ "", "  return (", "    ", "  );", "};", "" }),
-    rep(1),
-    t({ ".propTypes = {", "  " }),
-    rep(2),
-    t({ ",", "};", "", "export default " }),
-    rep(1),
-    t({ ";" }),
-  }),
-}
-
 require("luasnip.loaders.from_vscode").load()
 
 luasnip.snippets = {
-  javascript = javascript_snippets,
-  typescript = javascript_snippets,
+  javascript = require("snippets.javascript"),
+  typescript = require("snippets.javascript"),
 }
 
 vim.opt.completeopt = { "menu", "menuone", "noselect" }
-
-local has_words_before = function()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0
-    and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]
-        :sub(col, col)
-        :match("%s")
-      == nil
-end
-
-cmp.setup({
-  -- completion = {
-  --   autocomplete = false, -- disable auto-completion.
-  -- },
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body) -- For `luasnip` users.
-      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-    end,
-  },
-  mapping = {
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      elseif has_words_before() then
-        cmp.complete()
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-    ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
-    ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
-    ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-    ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-    ["<C-e>"] = cmp.mapping({
-      i = cmp.mapping.abort(),
-      c = cmp.mapping.close(),
-    }),
-    ["<CR>"] = cmp.mapping.confirm({
-      behavior = cmp.ConfirmBehavior.insert,
-      select = true,
-    }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-  },
-  sources = cmp.config.sources({
-    { name = "nvim_lsp" },
-    { name = "luasnip" }, -- For luasnip users.
-    -- { name = "ultisnips" }, -- For ultisnips users.
-  }, {
-    { name = "buffer" },
-  }),
-})
 
 -- Set configuration for specific filetype.
 cmp.setup.filetype("gitcommit", {
   sources = cmp.config.sources({
     { name = "cmp_git" }, -- You can specify the `cmp_git` source if you were installed it.
-  }, {
     { name = "buffer" },
   }),
-})
-
--- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline("/", {
-  sources = {
-    { name = "buffer" },
-  },
 })
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline(":", {
   sources = cmp.config.sources({
     { name = "path" },
-  }, {
     { name = "cmdline" },
   }),
 })
 
 -- Setup lspconfig.
-local capabilities = require("cmp_nvim_lsp").update_capabilities(
-  vim.lsp.protocol.make_client_capabilities()
-)
+-- local capabilities = require("cmp_nvim_lsp").update_capabilities(
+--   vim.lsp.protocol.make_client_capabilities()
+-- )
 -- -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
 -- require("lspconfig")["<YOUR_LSP_SERVER>"].setup({
 --   capabilities = capabilities,
 -- })
+
+cmp.setup({
+  completion = {
+    autocomplete = false, -- disable auto-completion.
+  },
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+  mapping = {
+      ['<C-u>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+      ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+      ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+      ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+      ['<Tab>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+      ['<C-e>'] = cmp.mapping({
+        i = cmp.mapping.abort(),
+        c = cmp.mapping.close(),
+      }),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    },
+    sources = cmp.config.sources({
+      { name = 'nvim_lua' },
+      -- { name = 'nvim_lsp' },
+      -- { name = 'vsnip' }, -- For vsnip users.
+      -- { name = 'luasnip' }, -- For luasnip users.
+      -- { name = 'ultisnips' }, -- For ultisnips users.
+      -- { name = 'snippy' }, -- For snippy users.
+      { name = 'buffer' },
+    })
+  })
+
+-- _G.vimrc = _G.vimrc or {}
+-- _G.vimrc.cmp = _G.vimrc.cmp or {}
+-- _G.vimrc.cmp.omni = function()
+--   cmp.complete({
+--     config = {
+--       sources = cmp.config.sources({
+--         { name = "nvim_lua" },
+--         -- { name = "nvim_lsp" },
+--         -- { name = "luasnip" }, -- For luasnip users.
+--         -- { name = "ultisnips" }, -- For ultisnips users.
+--         { name = "buffer" },
+--       }),
+--     },
+--   })
+-- end
+--
+-- vim.cmd([[
+--   inoremap <C-x><C-o> <Cmd>lua vimrc.cmp.omni()<CR>
+-- ]])
 
 require("cmp_git").setup({
   -- defaults
---
+++ nvim/lua/snippets/javascript.lua
@@ -0,0 +1,89 @@
+local luasnip = require("luasnip")
+local rep = require("luasnip.extras").rep
+local s = luasnip.s
+local t = luasnip.t
+local i = luasnip.i
+
+local javascript_snippets = {
+  s({ dscr = "{}", trig = "{}" }, { t({ "{", "  " }), i(0), t({ "", "}" }) }),
+  s({ dscr = "();", trig = "();" }, { t({ "(", "  " }), i(0), t({ "", ");" }) }),
+  s({ dscr = "{};", trig = "{};" }, { t({ "{", "  " }), i(0), t({ "", "};" }) }),
+  s({ dscr = "{});", trig = "{});" }, { t({ "{", "  " }), i(0), t({ "", "});" }) }),
+  s(
+    { dscr = "{}));", trig = "{}));" },
+    { t({ "{", "  " }), i(0), t({ "", "}));" }) }
+  ),
+  s({ dscr = "{},", trig = "{}," }, { t({ "{", "  " }), i(0), t({ "", "}," }) }),
+  s({ dscr = "('", trig = "('" }, { t({ "('" }), i(0), t({ "');" }) }),
+  s(
+    { dscr = "req", trig = "req" },
+    { t({ 'require("' }), i(0, "module"), t({ '");' }) }
+  ),
+  s({ dscr = "Describe", trig = "des" }, {
+    t({
+      'describe("',
+    }),
+    i(1, "description"),
+    t({ '", () => {', "  " }),
+    i(0),
+    t({ "", "});" }),
+  }),
+  s({ dscr = "it async", trig = "it" }, {
+    t({
+      'it("',
+    }),
+    i(1, "description"),
+    t({ '", async () => {', "  " }),
+    i(0),
+    t({ "", "});" }),
+  }),
+  s({ dscr = "it", trig = "its" }, {
+    t({
+      'it("',
+    }),
+    i(1, "description"),
+    t({ '", () => {', "  " }),
+    i(0),
+    t({ "", "});" }),
+  }),
+  s({ dscr = "before each", trig = "bef" }, {
+    t({ "beforeEach(() => {", "  " }),
+    i(0),
+    t({ "", "});" }),
+  }),
+  s({ dscr = "after each", trig = "af" }, {
+    t({ "afterEach(() => {", "  " }),
+    i(0),
+    t({ "", "});" }),
+  }),
+  s({ dscr = "before each async", trig = "befa" }, {
+    t({ "beforeEach(async () => {", "  " }),
+    i(0),
+    t({ "", "});" }),
+  }),
+  s({ dscr = "after each async", trig = "afa" }, {
+    t({ "afterEach(async () => {", "  " }),
+    i(0),
+    t({ "", "});" }),
+  }),
+  s({ dscr = "sfc", trig = "sfc" }, {
+    t({
+      "import React from 'react';",
+      "import PropTypes from 'prop-types';",
+      "",
+      "const ",
+    }),
+    i(1, "Component"),
+    t({ " = ({ " }),
+    i(2, "props"),
+    t({ " }) => {", "  " }),
+    i(0),
+    t({ "", "  return (", "    ", "  );", "};", "" }),
+    rep(1),
+    t({ ".propTypes = {", "  " }),
+    rep(2),
+    t({ ",", "};", "", "export default " }),
+    rep(1),
+    t({ ";" }),
+  }),
+}
