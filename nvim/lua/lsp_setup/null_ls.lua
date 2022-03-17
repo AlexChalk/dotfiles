2d925005e7dc81a5bb5c09886673e62a66c92bdc
Alex Chalk <contact@alexchalk.net>
2022-03-17 12:36:47 -0400

    Improve diagnostic/formatting config

--- nvim/lua/lsp_setup/null_ls.lua
+++ nvim/lua/lsp_setup/null_ls.lua
@@ -2,28 +2,33 @@
 local null_ls = require("null-ls")
 null_ls.setup({
   sources = {
-    -- null_ls.builtins.diagnostics.eslint.with({
-    --   prefer_local = "node_modules/.bin",
-    -- }),
-    null_ls.builtins.diagnostics.flake8.with({
-      command = require("commands.python").get_python_bin_path("flake8"),
+    null_ls.builtins.diagnostics.eslint.with({
+      prefer_local = "node_modules/.bin",
     }),
 
-    -- null_ls.builtins.formatting.reorder_python_imports.with({
-    --   command = require("commands.python").get_python_bin_path("reorder-python-imports"),
-    -- }),
+    null_ls.builtins.formatting.eslint.with({
+      prefer_local = "node_modules/.bin",
+    }),
 
-    -- null_ls.builtins.formatting.nixpkgs_fmt,
-    -- null_ls.builtins.formatting.latexindex,
-    -- null_ls.builtins.formatting.pgFormatter,
+    null_ls.builtins.diagnostics.flake8,
+
+    -- might need to pipx install this one
+    null_ls.builtins.formatting.reorder_python_imports,
 
     null_ls.builtins.formatting.black.with({
-      command = require("commands.python").get_python_bin_path("black"),
       extra_args = { "-v", "--config", "pyproject.toml" },
     }),
+
+    null_ls.builtins.formatting.nixpkgs_fmt,
+
+    -- null_ls.builtins.formatting.latexindent,
+
+    null_ls.builtins.formatting.pg_format,
+
     null_ls.builtins.formatting.shfmt.with({
       extra_args = { "-i", "2", "-ci" },
     }),
+
     null_ls.builtins.formatting.stylua.with({
       extra_args = { "--config-path", vim.fn.expand("$HOME/dotfiles/stylua.toml") },
     }),
