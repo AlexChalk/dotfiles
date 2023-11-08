bb7316f7f536e179404638b7fb8feb2b9059a90b
Alex Chalk <contact@alexchalk.net>
2023-11-08 17:57:51 -0500

    Update treesitter config

--- nvim/lua/conf/treesitter.lua
+++ nvim/lua/conf/treesitter.lua
@@ -1,5 +1,5 @@
 require("nvim-treesitter.configs").setup({
-  ensure_installed = { "clojure", "python", "hcl" }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = { "clojure", "python", "hcl", "lua", "c" }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   highlight = {
     enable = true, -- false will disable the whole extension
     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
