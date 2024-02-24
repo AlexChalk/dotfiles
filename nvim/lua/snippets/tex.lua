ff4f4d188f4dbc3a45c5176df9a37f9fe1bba37f
Alex Chalk <contact@alexchalk.net>
2024-02-23 19:29:56 -0500

    Improve tex snippets

--- nvim/lua/snippets/tex.lua
+++ nvim/lua/snippets/tex.lua
@@ -4,9 +4,28 @@ local t = luasnip.t
 local i = luasnip.i
 
 local snippets = {
-  s({ dscr = "italic", trig = "*" }, { t({ [[\textit{]] }), i(1), t({ "}" }), i(0) }),
-  s({ dscr = "quote", trig = [["]] }, { t({ [[\textquote{]] }), i(1), t({ "}" }), i(0) }),
-  s({ dscr = "block_quote", trig = [["""]] }, { t({ [[\blockquote{]] }), i(1), t({ "}" }), i(0) }),
+  s(
+    { dscr = "italic", trig = "*" },
+    { t({ [[\textit{]] }), i(1), t({ "}" }), i(0) }
+  ),
+  s(
+    { dscr = "quote", trig = [["]] },
+    { t({ [[\textquote{]] }), i(1), t({ "}" }), i(0) }
+  ),
+  s(
+    { dscr = "block_quote", trig = [["""]] },
+    { t({ [[\blockquote{]] }), i(1), t({ "}" }), i(0) }
+  ),
+  s({ dscr = "citation", trig = [[\cite]] }, {
+    t({
+      [[\cite[]],
+    }),
+    i(2, "pinpoint"),
+    t({ [[]{]] }),
+    i(1, ""),
+    t({ [[} ]] }),
+    i(0),
+  }),
 }
 
 return snippets
