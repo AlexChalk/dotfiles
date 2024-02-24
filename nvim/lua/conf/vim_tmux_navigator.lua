05ca462e0ced0dbb41e712db6a12cc2e6f5e0d19
Alex Chalk <contact@alexchalk.net>
2024-02-24 00:43:16 -0500

    Silence tmux-navigator mappings

--- nvim/lua/conf/vim_tmux_navigator.lua
+++ nvim/lua/conf/vim_tmux_navigator.lua
@@ -1,12 +1,32 @@
 vim.g.tmux_navigator_no_mappings = true
 
-vim.keymap.set({ "n" }, "<c-h>", ":<C-U>TmuxNavigateLeft<cr>", { remap = false })
-vim.keymap.set({ "n" }, "<c-j>", ":<C-U>TmuxNavigateDown<cr>", { remap = false })
-vim.keymap.set({ "n" }, "<c-k>", ":<C-U>TmuxNavigateUp<cr>", { remap = false })
-vim.keymap.set({ "n" }, "<c-l>", ":<C-U>TmuxNavigateRight<cr>", { remap = false })
+vim.keymap.set(
+  { "n" },
+  "<c-h>",
+  ":<C-U>TmuxNavigateLeft<cr>",
+  { remap = false, silent = true }
+)
+vim.keymap.set(
+  { "n" },
+  "<c-j>",
+  ":<C-U>TmuxNavigateDown<cr>",
+  { remap = false, silent = true }
+)
+vim.keymap.set(
+  { "n" },
+  "<c-k>",
+  ":<C-U>TmuxNavigateUp<cr>",
+  { remap = false, silent = true }
+)
+vim.keymap.set(
+  { "n" },
+  "<c-l>",
+  ":<C-U>TmuxNavigateRight<cr>",
+  { remap = false, silent = true }
+)
 vim.keymap.set(
   { "n" },
   [[<c-\>]],
   ":<C-U>TmuxNavigatePrevious<cr>",
-  { remap = false }
+  { remap = false, silent = true }
 )
