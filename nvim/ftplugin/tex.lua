22b195c074f109f1b32ea74a75f90ab78efd1462
Alex Chalk <contact@alexchalk.net>
2024-02-29 20:21:49 -0500

    Use human-readable jk movements in .tex files

--- nvim/ftplugin/tex.lua
+++ nvim/ftplugin/tex.lua
@@ -7,3 +7,6 @@ vim.keymap.set(
   "<cmd>Telescope bibtex format_string=%s<cr>",
   {}
 )
+
+vim.api.nvim_set_keymap("n", "j", "gj", { noremap = true })
+vim.api.nvim_set_keymap("n", "k", "gk", { noremap = true })
