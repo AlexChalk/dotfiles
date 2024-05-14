e6718f6d66900fc76bb83f32dcfacaf05cf622d3
Alex Chalk <contact@alexchalk.net>
2024-05-14 11:01:51 -0400

    Try adding linewise parinfer

--- nvim/ftplugin/clojure.lua
+++ nvim/ftplugin/clojure.lua
@@ -1,6 +1,17 @@
 -- -----------------------------------------------------
 -- -- Clojure
 -- -----------------------------------------------------
--- if vim.g.parinfer_mode == "smart" then
---   vim.g.parinfer_mode = "paren"
--- end
+-- Linewise parinfer (comments behaviour in comments.lua)
+vim.keymap.set(
+  "n",
+  "dd",
+  vim.call("parinfer#delete_line"),
+  { buffer = 0, noremap = true }
+)
+
+vim.keymap.set(
+  "n",
+  "p",
+  vim.call("parinfer#put_line"),
+  { buffer = 0, noremap = true }
+)
