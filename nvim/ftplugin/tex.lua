e1d8b1976fc2204b6db78ae9e6dcaa961cb8eba6
Alex Chalk <contact@alexchalk.net>
2024-05-14 11:01:09 -0400

    Use vim.keymap.set in tex.lua

--- nvim/ftplugin/tex.lua
+++ nvim/ftplugin/tex.lua
@@ -1,26 +1,11 @@
 -----------------------------------------------------
 -- Tex
 -----------------------------------------------------
+vim.keymap.set(
+  { "n", "s", "x" },
+  "<leader>pz",
+  "<cmd>Telescope bibtex format_string=%s<cr>",
+  { buffer = 0 }
+)
 vim.api.nvim_buf_set_keymap(0, "n", "j", "gj", { noremap = true })
 vim.api.nvim_buf_set_keymap(0, "n", "k", "gk", { noremap = true })
-vim.api.nvim_buf_set_keymap(
-  0,
-  "n",
-  "<leader>pz",
-  "<cmd>Telescope bibtex format_string=%s<cr>",
-  { noremap = true }
-)
-vim.api.nvim_buf_set_keymap(
-  0,
-  "s",
-  "<leader>pz",
-  "<cmd>Telescope bibtex format_string=%s<cr>",
-  { noremap = true }
-)
-vim.api.nvim_buf_set_keymap(
-  0,
-  "x",
-  "<leader>pz",
-  "<cmd>Telescope bibtex format_string=%s<cr>",
-  { noremap = true }
-)
