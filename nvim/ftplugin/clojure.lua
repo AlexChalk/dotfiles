fae5c115b5da046263dbd695df48b0204400821b
Alex Chalk <contact@alexchalk.net>
2024-05-14 12:19:57 -0400

    Run parinfer after all delete operations

--- nvim/ftplugin/clojure.lua
+++ nvim/ftplugin/clojure.lua
@@ -1,11 +1,21 @@
 -- -----------------------------------------------------
 -- -- Clojure
 -- -----------------------------------------------------
--- Linewise parinfer (comments behaviour in comments.lua)
-vim.keymap.set("n", "dd", function()
-  vim.call("parinfer#delete_line")
-end, { buffer = 0, noremap = true })
-
+-- Parinfer on put and delete (comments behaviour in comments.lua)
 vim.keymap.set("n", "p", function()
   vim.call("parinfer#put_line")
 end, { buffer = 0, noremap = true })
+
+local clojureparinfer =
+  vim.api.nvim_create_augroup("clojureparinfer", { clear = true })
+
+vim.api.nvim_create_autocmd("TextChanged", {
+  pattern = { "*.clj", "*.cljc", "*.cljs" },
+  callback = function()
+    local operation = vim.v.operator
+    if operation == "d" then
+      vim.call("parinfer#process_form")
+    end
+  end,
+  group = clojureparinfer,
+})
