0468c0936395ce2908f2e4c1dd608088b3e311b4
Alex Chalk <contact@alexchalk.net>
2024-05-16 11:11:09 -0400

    Disable runtime/ftplugin/markdown.vim shiftwidth

--- nvim/ftplugin/markdown.lua
+++ nvim/ftplugin/markdown.lua
@@ -1,6 +1,9 @@
 local opt = vim.bo
 
 opt.textwidth = 0
+opt.shiftwidth = 2
+
+vim.g.markdown_recommended_style = false
 
 vim.api.nvim_buf_set_keymap(0, "n", "j", "gj", { noremap = true })
 vim.api.nvim_buf_set_keymap(0, "n", "k", "gk", { noremap = true })
