cd3b9630cdcf3355baee2a16792d54cbab3289c4
Alex Chalk <contact@alexchalk.net>
2025-12-10 17:00:22 -0500

    Activate folds in .git files

---
+++ nvim/ftplugin/git.lua
@@ -0,0 +1,5 @@
+-----------------------------------------------------
+-- git
+-----------------------------------------------------
+vim.opt_local.foldmethod = "syntax"
+-- Open fold: zo; Close fold: zc, Toggle fold: za.
